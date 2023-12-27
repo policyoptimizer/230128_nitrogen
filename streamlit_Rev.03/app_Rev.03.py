a5e3da718c6e2c698ac6bfd70a2200acbd6b8c98
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2023-12-27 14:39:55 +0900

    a

--- streamlit_Rev.03/app_Rev.03.py
+++ streamlit_Rev.03/app_Rev.03.py
@@ -16,104 +16,27 @@ def load_data():
    url = 'https://drive.google.com/uc?id=1-1i_FLEQCP4MOL9VFg8fMwt2OLMscO7V'
    output = 'combined_df.csv'
    gdown.download(url, output, quiet=False)
-   return pd.read_csv(output)
+   df = pd.read_csv(output)
+   df['timestamp'] = pd.to_datetime(df['timestamp'])  # 'timestamp'를 datetime 형식으로 변환 (필요한 경우)
+   return df
 
-start_date = st.sidebar.date_input('시작 날짜', min_value=min(data['timestamp']), max_value=max(data['timestamp']), value=min(data['timestamp']))
-end_date = st.sidebar.date_input('종료 날짜', min_value=min(data['timestamp']), max_value=max(data['timestamp']), value=max(data['timestamp']))
+data = load_data()  # 데이터 로드
 
-# # 센서 목록을 데이터에서 동적으로 추출
-# sensors = ['FI_S_105.PV_Value']  # 예시, 실제 센서 목록으로 대체
-# for sensor in sensors:
-#    inputs[sensor] = st.number_input(sensor, value=0.0)
+# 날짜 선택 위젯
+start_date = st.sidebar.date_input(
+   '시작 날짜',
+   min_value=min(data['timestamp']),
+   max_value=max(data['timestamp']),
+   value=min(data['timestamp'])
+)
 
-# # 모델을 로드하는 함수 (실제 모델 파일 경로로 대체해야 함)
-# @st.cache(allow_output_mutation=True)
-# def load_model():
-#    model = joblib.load('path/to/your/model.pkl')  # 실제 모델 파일로 대체
-#    return model
+end_date = st.sidebar.date_input(
+   '종료 날짜',
+   min_value=min(data['timestamp']),
+   max_value=max(data['timestamp']),
+   value=max(data['timestamp'])
+)
 
-# # 앱에서 모델 사용
-# model = load_model()
-# if st.button('예측'):
-#    X_new = pd.DataFrame([list(inputs.values())], columns=list(inputs.keys()))
-#    y_pred = model.predict(X_new)
-#    st.write('예측된 종속 변수 값:', y_pred)
-
-# if st.button('예측'):
-#    # ... (예측 코드) ...
-#    st.write('예측된 종속 변수 값:', y_pred)
-#    st.bar_chart(y_pred)  # 예측 결과를 바 차트로 표시
-
-# try:
-#    # 데이터 로딩 및 예측 코드
-# except Exception as e:
-#    st.error(f"오류 발생: {e}")
-
-######
-# file_id = '1-1i_FLEQCP4MOL9VFg8fMwt2OLMscO7V'
-# url = f'https://drive.google.com/uc?id={file_id}'
-
-# # 임시 파일 경로
-# output = 'combined_df.csv'
-
-# # Google 드라이브에서 파일 다운로드
-# gdown.download(url, output, quiet=False)
-
-# # 데이터 불러오기
-# combined_df = pd.read_csv(output)
-
-# Streamlit 앱에 데이터를 표시합니다 (예시)
-# st.write(combined_df.head())
-######
-
-# # Streamlit 앱의 제목 설정
-# st.title('최적 질소 건조 SP 설정')
-
-# # 데이터 로드 (임시 데이터 또는 사용자가 업로드한 데이터를 사용할 수 있음)
-# # combined_df = pd.read_csv('path/to/your/data.csv')
-
-# # 데이터 로드 대신 임시 데이터 생성 (실제 앱에서는 실제 데이터를 로드해야 함)
-# combined_df = pd.DataFrame({
-#    'Timestamp': pd.date_range(start='1/1/2023', periods=100, freq='D'),
-#    'FI_S_105.PV_Value': np.random.rand(100) * 100,
-#    # ... 다른 센서 데이터 ...
-# })
-
-# # 1. 사용자가 특정 기간을 설정하면 그 기간동안의 시계열 그래프를 보여줌
-# st.subheader('1. 특정 기간의 시계열 그래프')
-# start_date = st.date_input('시작 날짜', value=pd.to_datetime('2023-01-01'))
-# end_date = st.date_input('종료 날짜', value=pd.to_datetime('2023-01-31'))
-
-# # 필터링된 데이터
-# filtered_df = combined_df[(combined_df['Timestamp'] >= start_date) & (combined_df['Timestamp'] <= end_date)]
-
-# # 시계열 그래프 그리기
-# st.line_chart(filtered_df.set_index('Timestamp'))
-
-# # 2. 사용자가 특정 기간을 설정하면 그 기간동안의 PI 와 FI 들의 센서들의 사분위수를 보여줌
-# st.subheader('2. 특정 기간의 센서 데이터 사분위수')
-# st.write(filtered_df.describe())
-
-# # 3. 사용자가 독립변수를 설정하면 종속변수의 예측값을 추출
-# st.subheader('3. 센서 데이터를 기반으로 예측하기')
-# # 사용자 입력 필드
-# fi_105_val = st.number_input('FI_S_105.PV_Value', value=50.0)
-# # ... 다른 센서 값 입력 필드 ...
-
-# # 예측을 위한 독립 변수 배열 생성
-# input_data = np.array([[fi_105_val,  # FI_S_105.PV_Value
-#                        # ... 다른 센서 값 ...
-#                        ]])
-
-# # 예측 모델 (이 부분은 실제 모델로 대체해야 함)
-# linear_model = LinearRegression()
-# rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-
-# # 이 부분에 모델 학습 코드 추가 (예: linear_model.fit(X, y))
-
-# # 예측 버튼
-# if st.button('예측하기'):
-#    predicted_linear = linear_model.predict(input_data)
-#    predicted_rf = rf_model.predict(input_data)
-#    st.write(f'선형 회귀 예측값: {predicted_linear[0]}')
-#    st.write(f'랜덤 포레스트 예측값: {predicted_rf[0]}')
+# 필터링된 데이터 프레임 생성 및 표시
+filtered_df = data[(data['timestamp'] >= start_date) & (data['timestamp'] <= end_date)]
+st.write(filtered_df)
